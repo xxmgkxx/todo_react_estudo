4f338505ae54dec6736503f50d745e250cd3ac76
Aluan <aluan.cabral@outlook.com>
2017-05-21 15:37:03 -0300

    Refatoração componente IconButton - Renderização Condicional -

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-const IconButton = (props) => {
-    if(props.hide) {
-        return null
-    } else {
-        return (
-            <button className={'btn btn-'+ props.style} onClick={props.onClick}>
-                <i className={'fa fa-'+ props.icon}></i>
-            </button>
+import If from './if'
+
+const IconButton = (props) => (
+            <If test={!props.hide}>
+                <button className={'btn btn-'+ props.style} onClick={props.onClick}>
+                    <i className={'fa fa-'+ props.icon}></i>
+                </button>
+            </If>
         )
-    }
-}
 
 export default IconButton
