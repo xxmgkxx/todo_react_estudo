ea2eb86ea6c1b6261d9ad3b91728e0ef80f389ac
Aluan <aluan.cabral@outlook.com>
2017-05-21 21:23:39 -0300

    Update CORS e Evento Adicionar Integrando com o Backend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props) {
@@ -16,7 +19,9 @@ export default class Todo extends Component {
         this.setState({ ...this.state, description: e.target.value })
     }
     handleAdd(){
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou'))
     }
     render() {
         return (
